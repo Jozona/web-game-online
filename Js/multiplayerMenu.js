a735a96f50a647af579a3cf1dd5890d6f4a63347
Josué Espinoza <69408723+Jozona@users.noreply.github.com>
2023-05-18 14:07:25 -0600

    Add files via upload

---
+++ Js/multiplayerMenu.js
@@ -0,0 +1,17 @@
+function crearPartida(){
+    location.href = "../multijugador.html?room=" + makeid(5);
+}
+
+
+//Generar el codigo
+function makeid(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
